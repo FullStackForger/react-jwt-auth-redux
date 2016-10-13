d8c09c9584995c93fbb1a22c1b3b4bc28a65c473
FullStackForger <fullstackforger@gmail.com>
2016-10-13 17:19:55 +0100

    index.spec mirrored from react-jwt-auth

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,15 +1,77 @@
 import chai from 'chai'
-import app from './index'
+import jwtAuth from './index'
+import {Auth, Google, Facebook} from './index'
 
 const expect = chai.expect
+const assert = chai.assert
 
-describe('app', () => {
+describe('react-jwt-auth', () => {
 
   it('should exist', () => {
-    expect(app).to.exist
+    expect(jwtAuth).to.exist
   })
 
-  it('should have foo', () => {
-    expect(app.foo).to.equal('bar')
+
+
+  describe('components', () => {
+    it('should expose Auth', () => {
+      assert.isFunction(Auth)
+      assert.isFunction(jwtAuth.Auth)
+    })
+
+    it('should expose Facebook', () => {
+      assert.isFunction(Facebook)
+      assert.isFunction(jwtAuth.Facebook)
+    })
+
+    it('should expose Google', () => {
+      assert.isFunction(Google)
+      assert.isFunction(jwtAuth.Google)
+    })
   })
+
+  describe('api methods', () => {
+
+    it('should expose isAuthenticated', () => {
+      assert.isFunction(jwtAuth.isAuthenticated)
+    })
+
+    it('should expose login', () => {
+      assert.isFunction(jwtAuth.login)
+    })
+
+    it('should expose logout', () => {
+      assert.isFunction(jwtAuth.logout)
+    })
+
+    it('should expose signup', () => {
+      assert.isFunction(jwtAuth.signup)
+    })
+
+    it('should expose getToken', () => {
+      assert.isFunction(jwtAuth.getToken)
+    })
+
+    it('should expose setToken', () => {
+      assert.isFunction(jwtAuth.setToken)
+    })
+
+    it('should expose refreshToken', () => {
+      assert.isFunction(jwtAuth.refreshToken)
+    })
+
+    it('should expose getAuthHeader', () => {
+      assert.isFunction(jwtAuth.getAuthHeader)
+    })
+
+    it('should expose getProfile', () => {
+      assert.isFunction(jwtAuth.getProfile)
+    })
+
+    it('should expose updateProfile', () => {
+      assert.isFunction(jwtAuth.updateProfile)
+    })
+  })
+
+
 })
